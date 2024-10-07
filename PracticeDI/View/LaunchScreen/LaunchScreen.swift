194c42db9b1312b71b1dc49d8112a43b94721579
alibekshak <alibekshakiro1@gmail.com>
2024-10-07 12:14:08 +0500

    Crate LaunchScreen

---
+++ PracticeDI/View/LaunchScreen/LaunchScreen.swift
@@ -0,0 +1,33 @@
+//
+//  LaunchScreen.swift
+//  PracticeDI
+//
+//  Created by Alibek Shakirov on 07.10.2024.
+//
+
+import SwiftUI
+
+struct LaunchScreen: View {
+    
+    @State private var isAnimating = false
+    
+    var body: some View {
+        ZStack {
+            Color.blue.blur(radius: 35)
+            Image(systemName: "photo")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 250, height: 250)
+                .scaleEffect(isAnimating ? 1 : 0.5)
+                .animation(.easeInOut(duration: 1.5), value: isAnimating)
+        }
+        .ignoresSafeArea()
+        .onAppear {
+            self.isAnimating = true
+        }
+    }
+}
+
+#Preview {
+    LaunchScreen()
+}
