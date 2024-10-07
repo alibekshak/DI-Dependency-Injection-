186bb0c43a3bf523f0f9e433edaa08e4f6bbc8f2
alibekshak <alibekshakiro1@gmail.com>
2024-10-07 11:42:47 +0500

    first commit

---
+++ PracticeDI/PracticeDIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeDIApp.swift
+//  PracticeDI
+//
+//  Created by Alibek Shakirov on 07.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct PracticeDIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
