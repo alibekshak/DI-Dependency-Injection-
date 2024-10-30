3c8f3567e8a42484fb6d15a280ccd6a67311f8ea
alibekshak <alibekshakiro1@gmail.com>
2024-10-30 14:18:44 +0500

    New UI for ContactInfo

--- PracticeDI/View/InfoAddresses/Auxiliary/ContactInfo.swift
+++ PracticeDI/View/InfoAddresses/Auxiliary/ContactInfo.swift
@@ -16,8 +16,36 @@ struct ContactInfo: View {
     var body: some View {
         VStack(spacing: .zero) {
             navigationBar
-            Text("\(contact.firstname ?? "") \(contact.lastname ?? "")")
+            VStack(alignment: .center, spacing: 12) {
+                Text("\(contact.firstname ?? "") \(contact.lastname ?? "")")
+                    .font(.system(size: 20, weight: .semibold))
+                HStack(alignment: .center) {
+                    HStack(spacing: 2) {
+                        Text("Contact Info")
+                        Image(systemName: "info.circle")
+                    }
+                    
+                    Spacer()
+                    
+                    VStack(alignment: .trailing, spacing: 6) {
+                        Text(contact.email ?? "")
+                        Text(contact.phone ?? "")
+                    }
+                }
+                .padding(.horizontal, 12)
+                
+                Divider()
+                
+                HStack(alignment: .top) {
+                    Text("Working time")
+                    Spacer()
+                    Text("9.00 AM : 5.00 PM")
+                }
+                .padding(.horizontal, 12)
+            }
+            Spacer()
         }
+        .font(.system(size: 18, weight: .medium))
         .padding(.horizontal, 12)
     }
     
@@ -28,6 +56,7 @@ struct ContactInfo: View {
         .font(.system(size: 20, weight: .bold ,design: .rounded))
         .foregroundStyle(colorScheme == .dark ? Color.white : Color.black)
         .padding(.bottom, 20)
+        .padding(.top)
     }
 }
 
