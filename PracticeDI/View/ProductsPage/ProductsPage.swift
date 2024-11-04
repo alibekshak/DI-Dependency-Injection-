f1393ba9d239e34849b853eb0ec0eef56a2f44b7
alibekshak <alibekshakiro1@gmail.com>
2024-11-04 10:13:15 +0500

    Add loadingView to ProductsPage

--- PracticeDI/View/ProductsPage/ProductsPage.swift
+++ PracticeDI/View/ProductsPage/ProductsPage.swift
@@ -21,19 +21,35 @@ struct ProductsPage: View {
     
     var body: some View {
         VStack(spacing: .zero) {
-            ScrollView(showsIndicators: false) {
-                LazyVGrid(columns: columns, spacing: 12) {
-                    ForEach(viewModel.products, id: \.self) { product in
-                        ProductCard(info: product)
-                    }
-                }
+            if viewModel.isLoading {
+                loadingView
+            } else {
+                products
             }
-            Spacer()
         }
         .padding(.horizontal, 12)
         .background(Color(.systemGray6))
         .onAppear {
             viewModel.getProducts()
         }
+    }
+    
+    var products: some View {
+        ScrollView(showsIndicators: false) {
+            LazyVGrid(columns: columns, spacing: 12) {
+                ForEach(viewModel.products, id: \.self) { product in
+                    ProductCard(info: product)
+                }
+            }
+            .padding(.bottom)
+        }
+    }
+    
+    var loadingView: some View {
+        VStack {
+            ProgressView()
+                .foregroundStyle(Color(uiColor: .label))
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
     }
 }
